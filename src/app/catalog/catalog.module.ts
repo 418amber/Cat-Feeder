d1a4ea4a9ae0796b7002286226708ac9aecfbf79
Amber Chen <amber.chen@aveva.com>
2024-07-09 15:54:38 -0400

    scroll to top button

--- src/app/catalog/catalog.module.ts
+++ src/app/catalog/catalog.module.ts
@@ -4,15 +4,19 @@ import { CatalogComponent } from './components/catalog/catalog.component';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { ScrollToTopComponent } from './components/scroll-to-top/scroll-to-top.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    CatalogComponent
+    CatalogComponent,
+    ScrollToTopComponent,
   ],
   imports: [
     CommonModule,
     MatCardModule,
     InfiniteScrollModule,
+    MatButtonModule,
     RouterModule.forChild([
       { path: '', component: CatalogComponent }
     ]),
